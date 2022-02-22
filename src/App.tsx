48b706ae4dd336f0447e9869b4526417565204a1
paulo.vieira <paulo.vieira@acct.global>
2022-02-22 18:02:57 -0300

    Ajusts on layout start implement customers block

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Description } from './components/Description';
-import { Header } from './components/Header';
-import { Introduction } from './components/Introduction';
-import './styles/global.scss'
+import React from "react";
+import { Customers } from "./components/Customers";
+import { Description } from "./components/Description";
+import { Header } from "./components/Header";
+import { Introduction } from "./components/Introduction";
+import "./styles/global.scss";
 
 function App() {
   return (
-   <>
-    <Header/>
-    <Description/>
-    <Introduction/>
-   </>
+    <>
+      <Header />
+      <Description />
+      <Introduction />
+      <Customers/>
+    </>
   );
 }
 
--- src/components/Customers/index.tsx
+++ src/components/Customers/index.tsx
@@ -2,6 +2,15 @@ import styles from './styles.module.scss'
 
 export function Customers(){
     return (
-        <div></div>
+        <section className={styles.customerContainer}>
+            <img className={styles.wavesImage} src="/images/waves.svg" alt="ondas"/>
+            <div className={styles.titleContainer}>
+                <h1><span>Alunas que fizeram e</span> Aprovam o curso</h1>
+                <p>Você vai ser a próxima</p>
+
+            </div>
+
+
+        </section>
     )
 }
